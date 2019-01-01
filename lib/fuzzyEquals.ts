9f7daaf35798b8eb99015400955c2663348b77c5
Nathan Faucett <nathanfaucett@gmail.com>
2019-01-01 11:18:58 -0500

    clean up

--- lib/fuzzyEquals.ts
+++ lib/fuzzyEquals.ts
@@ -1,43 +1,43 @@
 export const fuzzyEquals = (
-    query: string,
-    text: string,
-    caseSensitive: boolean = false
+  query: string,
+  text: string,
+  caseSensitive: boolean = false
 ) => {
-    const textLength = text.length,
-        queryLength = query.length;
+  const textLength = text.length,
+    queryLength = query.length;
 
-    if (queryLength > textLength) {
-        return false;
-    } else {
-        return fuzzyEqualsImpl(
-            caseSensitive !== true ? query.toLowerCase() : query,
-            queryLength,
-            caseSensitive !== true ? text.toLowerCase() : text,
-            textLength
-        );
-    }
+  if (queryLength > textLength) {
+    return false;
+  } else {
+    return fuzzyEqualsImpl(
+      caseSensitive !== true ? query.toLowerCase() : query,
+      queryLength,
+      caseSensitive !== true ? text.toLowerCase() : text,
+      textLength
+    );
+  }
 };
 
 export const fuzzyEqualsImpl = (
-    query: string,
-    queryLength: number,
-    text: string,
-    textLength: number
+  query: string,
+  queryLength: number,
+  text: string,
+  textLength: number
 ) => {
-    let i = 0,
-        j = 0;
+  let i = 0,
+    j = 0;
 
-    outer: while (i < queryLength) {
-        const ch = query.charAt(i++);
+  outer: while (i < queryLength) {
+    const ch = query.charAt(i++);
 
-        while (j < textLength) {
-            if (text.charAt(j++) === ch) {
-                continue outer;
-            }
-        }
-
-        return false;
+    while (j < textLength) {
+      if (text.charAt(j++) === ch) {
+        continue outer;
+      }
     }
 
-    return true;
+    return false;
+  }
+
+  return true;
 };
